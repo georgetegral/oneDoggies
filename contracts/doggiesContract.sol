23a3aa8b0594faaaf8017d433b455dc825453fe9
georgetegral <jorgerenemg@icloud.com>
2022-06-07 20:52:04 -0500

    Added base contract

--- contracts/doggiesContract.sol
+++ contracts/doggiesContract.sol
@@ -6,4 +6,23 @@ import "./IERC721.sol";
 
 contract DoggiesContract is IERC721 {
 
+    string public constant name = "ONEDoggies";
+    string public constant symbol = "DOGGIES";
+
+    struct Doggie {
+        uint256 genes;
+        uint64 birthTime;
+        uint32 momId;
+        uint32 dadId;
+        uint16 generation;
+    }
+
+    Doggie[] doggies;
+    mapping(uint256 => address) public kittyIndexToOwner;
+    mapping(address => uint256) ownershipTokenCount;
+
+    function balanceOf(address owner) external view returns (uint256 balance){
+        return ownershipTokenCount[owner];
+    }
+
 }
