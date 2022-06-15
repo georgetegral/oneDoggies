04aca3a71c3fb6de098f6783b0e218cc44bdca2e
georgetegral <jorgerenemg@icloud.com>
2022-06-15 12:34:46 -0500

    Added create doggie function and birth event

--- contracts/doggiesContract.sol
+++ contracts/doggiesContract.sol
@@ -9,6 +9,14 @@ contract DoggiesContract is IERC721 {
     string public constant name = "ONEDoggies";
     string public constant symbol = "DOGGIES";
 
+    event Birth(
+        address owner, 
+        uint256 doggieId, 
+        uint256 momId, 
+        uint256 dadId, 
+        uint256 genes
+    );
+
     struct Doggie {
         uint256 genes;
         uint64 birthTime;
@@ -20,6 +28,22 @@ contract DoggiesContract is IERC721 {
     Doggie[] doggies;
     mapping(uint256 => address) public doggieIndexToOwner;
     mapping(address => uint256) ownershipTokenCount;
+
+    function _createDoggie(uint256 _momId, uint256 _dadId, uint256 _generation, uint256 _genes, address _owner) private returns (uint256) {
+        Doggie memory _doggie = Doggie({
+            genes: _genes,
+            birthTime: uint64(block.timestamp),
+            momId: uint32(_momId),
+            dadId: uint32(_dadId),
+            generation: uint16(_generation)
+        });
+
+        doggies.push(_doggie);
+        uint256 newDoggieId = doggies.length - 1;
+        emit Birth(_owner, newDoggieId, _momId, _dadId, _genes);
+        _transfer(address(0), _owner, newDoggieId);
+        return newDoggieId;
+    }
 
     function balanceOf(address owner) external view returns (uint256 balance){
         return ownershipTokenCount[owner];
