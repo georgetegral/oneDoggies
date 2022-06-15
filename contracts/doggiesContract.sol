27356d238e9f336adddedeca9d07878bc7343761
georgetegral <jorgerenemg@icloud.com>
2022-06-15 11:37:16 -0500

    Added functionality to contract

--- contracts/doggiesContract.sol
+++ contracts/doggiesContract.sol
@@ -25,4 +25,36 @@ contract DoggiesContract is IERC721 {
         return ownershipTokenCount[owner];
     }
 
+    function totalSupply() public view returns (uint256){
+        return doggies.length;
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address){
+        return doggieIndexToOwner[_tokenId];
+    }
+
+    function transfer(address _to, uint256 _tokenId) external{
+        require(_to != address(0));
+        require(_to != address(this));
+        require(_owns(msg.sender, _tokenId));
+
+        _transfer(msg.sender, _to, _tokenId);
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) internal {
+        ownershipTokenCount[_to]++;
+        doggieIndexToOwner[_tokenId] = _to;
+        
+        if(_from != address(0)){
+            ownershipTokenCount[_from]--;
+        }
+
+        //Emit the transfer event
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function _owns(address _claimant, uint256 _tokenId) internal view returns (bool){
+        return doggieIndexToOwner[_tokenId] == _claimant;
+    }
+
 }
