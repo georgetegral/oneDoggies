9b761bc77c2d9ee594a5e9ae1325b68f5ded693c
georgetegral <jorgerenemg@icloud.com>
2022-06-17 20:15:46 -0500

    removed previous project unused files

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -11,63 +11,12 @@ import {
   import { ExternalLinkIcon } from '@chakra-ui/icons'
   import { Link } from "react-router-dom";
   import { useWeb3React } from "@web3-react/core";
-  import useRandomAvatars from "../../hooks/useRandomAvatars";
   import { useCallback, useEffect, useState } from "react";
   
   const Home = () => {
     const [isMinting, setIsMinting] = useState(false);
-    const [availableAvatars, setAvailableAvatars] = useState("");
     const { account } = useWeb3React();
-    const randomAvatars = useRandomAvatars();
     const toast = useToast();
-  
-    const getRandomAvatarsData = useCallback(async () => {
-      if (randomAvatars) {
-        const totalSupply = await randomAvatars.methods.totalSupply().call();
-
-        const maxSupply = await randomAvatars.methods.maxSupply().call();
-        setAvailableAvatars(maxSupply - totalSupply); 
-
-      }
-    }, [randomAvatars]);
-  
-    useEffect(() => {
-      getRandomAvatarsData();
-    }, [getRandomAvatarsData]);
-  
-    const mint = () => {
-        setIsMinting(true);
-
-        randomAvatars.methods
-        .mint()
-        .send({
-            from: account
-            //value: 1e15
-        })
-        .on("transactionHash", (txHash) => {
-            toast({
-                title: "Transacción enviada",
-                description: txHash,
-                status: "info",
-            })
-        })
-        .on("receipt", () => {
-            toast({
-                title: "Transacción confirmada",
-                description: "Se ha minteado correctamente el avatar",
-                status: "success",
-            })
-        })
-        .on("error", (error) => {
-            toast({
-                title: "Transacción fallida",
-                description: error.message,
-                status: "error",
-            })
-        })
-
-        setIsMinting(false);
-    }
 
     return (
       <Stack
@@ -103,27 +52,11 @@ import {
              Ver la galería en OpenSea. <ExternalLinkIcon mx='2px' />
             </LinkChakra>
           </Text>
-          <Text color={"blue.700"}>
-            Avatars disponibles : {availableAvatars}
-          </Text>
           <Stack
             spacing={{ base: 4, sm: 6 }}
             direction={{ base: "column", sm: "row" }}
           >
-            <Button
-              rounded={"full"}
-              size={"lg"}
-              fontWeight={"normal"}
-              px={6}
-              colorScheme={"blue"}
-              bg={"blue.400"}
-              _hover={{ bg: "blue.500" }}
-              disabled={!randomAvatars}
-              onClick={mint}
-              isLoading={isMinting}
-            >
-              Obtén tu avatar
-            </Button>
+            
             <Link to="/avatars">
               <Button rounded={"full"} size={"lg"} fontWeight={"normal"} px={6}>
                 Galería
